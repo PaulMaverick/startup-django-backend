741b9d0518ab888e17b7331f9160040c28b764ee
Paul <paulpastor122297@gmail.com>
2025-03-11 21:28:09 +0800

    initial models

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class JobPosition(models.Model):
+    job_id = models.AutoField(primary_key=True)
+    job_name = models.CharField(max_length=100)
+    job_description = models.TextField()
+    
+class Departments(models.Model):
+    dept_id = models.AutoField(primary_key=True)
+    dept_name = models.CharField(max_length=100)
+
+class Employees(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    job_id = models.ForeignKey(JobPosition, on_delete=models.SET_NULL, null=True, blank=True)
+    dept_id = models.ForeignKey(Departments, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    
+    
